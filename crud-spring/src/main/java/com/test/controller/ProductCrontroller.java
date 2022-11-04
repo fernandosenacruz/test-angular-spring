80da530fc7fa1f0d199ee3d71b0cb87df47c0863
fernandosenacruz <fernandosenacruz@gmail.com>
2022-11-04 08:30:43 -0300

    feat: :sparkles: GetById
    Cria rota de retorno de um produto por id

--- crud-spring/src/main/java/com/test/controller/ProductCrontroller.java
+++ crud-spring/src/main/java/com/test/controller/ProductCrontroller.java
@@ -3,7 +3,9 @@ package com.test.controller;
 import java.util.List;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,13 +25,20 @@ public class ProductCrontroller {
   }
 
   @GetMapping
-  public List<Product> getProducts() {
+  List<Product> getProducts() {
     return productRepository.findAll();
   }
 
-  @PostMapping("/post")
+  @GetMapping("/${id}")
+  ResponseEntity<Product> getById(@PathVariable Long id) {
+    return productRepository.findById(id)
+      .map(product ->  ResponseEntity.ok().build(product))
+      .orElse(ResponseEntity.notFound().build());
+  }
+
+  @PostMapping
   @ResponseStatus(code = HttpStatus.CREATED)
-  public Product create(@RequestBody Product product) {
+  Product create(@RequestBody Product product) {
     return productRepository.save(product);
   }
 }
