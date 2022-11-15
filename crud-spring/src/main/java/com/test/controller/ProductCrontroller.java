ebf31739a105c4f578154a92100ea8ef7aa30a2e
fernandosenacruz <fernandosenacruz@gmail.com>
2022-11-15 14:36:55 -0300

    feat(CRUD): :sparkles: Update e Delete
    Finaliza o CRUD com métodos de atualização e deleção de produtos

--- crud-spring/src/main/java/com/test/controller/ProductCrontroller.java
+++ crud-spring/src/main/java/com/test/controller/ProductCrontroller.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -25,20 +27,49 @@ public class ProductCrontroller {
   }
 
   @GetMapping
-  List<Product> getProducts() {
+  public List<Product> getProducts() {
     return productRepository.findAll();
   }
 
   @GetMapping("/${id}")
-  ResponseEntity<Product> getById(@PathVariable Long id) {
+  public ResponseEntity<Product> getProductById(@PathVariable Long id) {
     return productRepository.findById(id)
-      .map(product ->  ResponseEntity.ok().build(product))
-      .orElse(ResponseEntity.notFound().build());
+        .map(product -> ResponseEntity.ok().body(product))
+        .orElse(ResponseEntity.notFound().build());
   }
 
   @PostMapping
   @ResponseStatus(code = HttpStatus.CREATED)
-  Product create(@RequestBody Product product) {
+  public Product create(@RequestBody Product product) {
     return productRepository.save(product);
+  }
+
+  @PutMapping("/${id}")
+  public ResponseEntity<Product> update(
+      @PathVariable() Long id,
+      @RequestBody Product product) {
+    return productRepository.findById(id)
+        .map(p -> {
+          p.setName(p.getName());
+          p.setDescription(p.getDescription());
+          p.setUrlImage(p.getUrlImage());
+          p.setUnitValue(p.getUnitValue());
+
+          Product updatedProduct = productRepository.save(p);
+
+          return ResponseEntity.ok().body(updatedProduct);
+        })
+        .orElse(ResponseEntity.notFound().build());
+  }
+
+  @DeleteMapping("/${id")
+  public ResponseEntity<Void> delete(@PathVariable() Long id) {
+    return productRepository.findById(id)
+        .map(product -> {
+          productRepository.deleteById(id);
+
+          return ResponseEntity.noContent().<Void>build();
+        })
+        .orElse(ResponseEntity.notFound().build());
   }
 }
