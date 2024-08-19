afcd585aa0176a9ec832fe545e8670da86df6a61
Fernando Sena <fernando.sena@mkdata.com.br>
2024-08-19 14:53:19 -0300

    update dependencies

--- crud-angular/src/app/products/guards/product.resolver.ts
+++ crud-angular/src/app/products/guards/product.resolver.ts
@@ -1,18 +1,13 @@
 import { Product } from './../containers/products/interfaces/product';
 import { ProductsService } from './../services/products.service';
 import { Injectable } from '@angular/core';
-import {
-  Router,
-  Resolve,
-  RouterStateSnapshot,
-  ActivatedRouteSnapshot,
-} from '@angular/router';
+import { Router, RouterStateSnapshot, ActivatedRouteSnapshot } from '@angular/router';
 import { Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
-export class ProductResolver implements Resolve<Product> {
+export class ProductResolver  {
   constructor(private service: ProductsService) {}
 
   resolve(
