ffb86c006eaee8cf656f400ba850f449a8eaa190
Fernando Sena <fernando.sena@mkdata.com.br>
2024-08-19 18:39:57 -0300

    fix: adjusts angular v18

--- crud-angular/src/app/app.module.ts
+++ crud-angular/src/app/app.module.ts
@@ -7,11 +7,12 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductsModule } from './products/products.module';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 @NgModule({ declarations: [AppComponent],
     bootstrap: [AppComponent], imports: [BrowserModule,
         AppRoutingModule,
         BrowserAnimationsModule,
         MatToolbarModule,
-        ProductsModule], providers: [provideHttpClient(withInterceptorsFromDi())] })
+        ProductsModule], providers: [provideHttpClient(withInterceptorsFromDi()), provideAnimationsAsync()] })
 export class AppModule {}
